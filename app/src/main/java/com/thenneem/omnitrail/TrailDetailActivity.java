2e60f937ed2f8b11039753f5006437a2fa41ecce
theneem@gmail.com <theneem@gmail.com>
2021-06-12 10:46:47 -0400

    changed google map from map view to to fragment

--- app/src/main/java/com/thenneem/omnitrail/TrailDetailActivity.java
+++ app/src/main/java/com/thenneem/omnitrail/TrailDetailActivity.java
@@ -13,10 +13,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-public class TrailDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+public class TrailDetailActivity extends AppCompatActivity implements OnMapReadyCallback  {
 
     private BottomSheetBehavior mBottomSheetBehavior;
     private MapView mapView;
+
+
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +38,30 @@ public class TrailDetailActivity extends AppCompatActivity implements OnMapReady
         setContentView(R.layout.activity_trail_detail);
 
       //  View bottomSheet = findViewById(R.id.bottomSheetScroll);
-        mapView = findViewById(R.id.mapView);
-        mapView.getMapAsync(this);
-        mapView.onCreate(savedInstanceState);
+      //  mapView = findViewById(R.id.mapView);
+      //  mapView.getMapAsync(this);
+      //  mapView.onCreate(savedInstanceState);
 //        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 //        mBottomSheetBehavior.setPeekHeight(300);
 //        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
      }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 }
